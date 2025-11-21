1b8f1e9a7236736b6aae7b5d237b4692211f98d1
Matei David <davidmatei1902@gmail.com>
2025-11-21 19:42:06 +0200

    Implemented pwm_gen module FINALLY

--- src/pwm_gen.v
+++ src/pwm_gen.v
@@ -12,5 +12,43 @@ module pwm_gen (
     // top facing signals
     output pwm_out
 );
+    reg pwm_logic_out;
+    reg pwm_out_reg;
     
+    always @(*) begin
+        // check unaligned mode (window)
+        if (functions[1]) begin
+            // square window pwm logic - 001111100
+            if (count_val >= compare1 && count_val < compare2) begin
+                pwm_logic_out = 1'b1;
+            end else begin
+                pwm_logic_out = 1'b0;
+            end
+        end else begin
+            // aligned mode
+            if (functions[0] == 1'b0) begin
+                //  left alignment - 111110000
+                pwm_logic_out = (count_val < compare1) ? 1'b1 : 1'b0;
+            end else begin
+                // right alignment - 000001111
+                pwm_logic_out = (count_val < compare1) ? 1'b0 : 1'b1;
+            end
+        end
+    end
+
+    // clk sync
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            pwm_out_reg <= 1'b0;
+        end else begin
+            // update exit if pwm is enable - rest of logic above
+            if (pwm_en) begin
+                pwm_out_reg <= pwm_logic_out;
+            end else begin
+                pwm_out_reg <= 1'b0;
+            end
+        end
+    end
+
+    assign pwm_out = pwm_out_reg;
 endmodule
